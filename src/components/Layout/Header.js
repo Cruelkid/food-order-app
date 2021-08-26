0aecf41b2094418addc3dfca58449030e6f0a2c7
Cruelkid <sonofammit@gmail.com>
2021-08-26 15:55:45 +0300

    completed header cart button comopnent

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,16 +2,17 @@ import { Fragment } from "react";
 
 import mealsImage from "../../assets/meals.jpg";
 import styles from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
     return (
         <Fragment>
             <header className={styles.header}>
                 <h1>Meals</h1>
-                <button>Cart</button>
+                <HeaderCartButton />
             </header>
             <div className={styles["main-image"]}>
-                <img src={mealsImage} alt="image is missing :(" />
+                <img src={mealsImage} alt="oops! something is missing :(" />
             </div>
         </Fragment>
     );
