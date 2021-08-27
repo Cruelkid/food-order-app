022c126529635c241b04f5e950d5c8db33918913
Cruelkid <sonofammit@gmail.com>
2021-08-27 21:37:06 +0300

    expanded cart provider

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context";
 import styles from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+    const cartCtx = useContext(CartContext);
+
+    const cartItemsCount = cartCtx.items.reduce((currentNumber, item) => {
+        return currentNumber + item.amount;
+    }, 0);
+
     return (
         <button className={styles.button} onClick={props.onClick}>
             <span className={styles.icon}>
                 <CartIcon />
             </span>
             <span>Your cart</span>
-            <span className={styles.badge}>7</span>
+            <span className={styles.badge}>{cartItemsCount}</span>
         </button>
     );
 };
